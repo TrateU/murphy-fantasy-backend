ad1721906210a90b16eeda3494d2085824aa3fe8
Trate Underwood <trateu@stevens.edu>
2026-09-02 11:32:44 -0400

    2026 Initial

--- getTeamStats.py
+++ getTeamStats.py
@@ -87,7 +87,7 @@ def getStats(year):
             if year < info.currentYear:
                 isPast = True
             else:
-                for weeks in info.weekdates2025:
+                for weeks in info.weekdates2026:
                     if weeks['Week'] == score['Week']:
                         if today >= datetime.strptime(weeks['end'],"%Y-%m-%d"): 
                             isPast = True
